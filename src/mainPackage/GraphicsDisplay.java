2ced0b2e6270ec37260d0c91140532f07782e80c
Egor <etretiak03@gmail.com>
2021-12-15 06:27:17 +0300

    add: class 'MainFrame'

--- src/mainPackage/GraphicsDisplay.java
+++ src/mainPackage/GraphicsDisplay.java
@@ -1,19 +1,12 @@
 package mainPackage;
 
-import java.awt.BasicStroke;
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Paint;
-import java.awt.Stroke;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.font.FontRenderContext;
-import java.awt.geom.Ellipse2D;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
-import javax.swing.JPanel;
 
 public class GraphicsDisplay extends JPanel {
 
